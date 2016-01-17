d3729922c21d8b2dc6c18ceb8b8a3a58bc57f9a8
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-01-17 21:41:43 +0000

    Migliorati i menu

--- tanzobot.py
+++ tanzobot.py
@@ -11,20 +11,24 @@ import os
 emoji = telegram.Emoji
 bouncetime = 200
 
-help_text= (
-			"Benvenuto ! Io sono @TanzoBot..." + emoji.SMILING_FACE_WITH_SUNGLASSES + "\n"
-			"Menu dei comandi disponibili:\n"
-			"/help Elenco comandi\n"
-			"/luci Attuazione rele\n"
-			"/foto Scatta una foto da webcam\n"
-			"/video Riprende 10s di video da webcam\n"
-			"/webimage Immagine presa da web\n"
-			"/pdf Invia un PDF memorizzato su microSD\n"
-			"/mp3 Invia un MP3 memorizzato su microSD\n"
-			"/dog Riproduce il ringhio di un cane sugli speaker\n" 
-			"/mute Blocca omxplayer\n"
-			)
-			
+welcome_text = 	"Benvenuto ! Io sono @TanzoBot..." + emoji.SMILING_FACE_WITH_SUNGLASSES + "\n" 
+
+menu_keyboard = telegram.ReplyKeyboardMarkup([['/luci','/foto','/video','/logo','/start'],['/dog','/warner','/mute','/pdf','/menu']])
+menu_keyboard.one_time_keyboard=False
+menu_keyboard.resize_keyboard=True
+
+luci_keyboard = telegram.ReplyKeyboardMarkup([[ 'SX ON', 'DX ON' ],[ 'SX OFF', 'DX OFF' ], ['/menu']])
+luci_keyboard.one_time_keyboard=False
+luci_keyboard.resize_keyboard=True
+
+foto_keyboard = telegram.ReplyKeyboardMarkup([['CAMERA 1','CAMERA 2','CAMERA 3'],['/menu' ]])
+foto_keyboard.one_time_keyboard=False
+foto_keyboard.resize_keyboard=True
+
+video_keyboard = telegram.ReplyKeyboardMarkup([['VCAMERA 1','VCAMERA 2','VCAMERA 3'],['/menu' ]])
+video_keyboard.one_time_keyboard=False
+video_keyboard.resize_keyboard=True
+	
 # Enable logging
 logging.basicConfig(
 		format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -41,41 +45,29 @@ def switch_handler(channel):
 
 
 def start(bot, update):
-	bot.sendMessage(update.message.chat_id, text=help_text)
+	bot.sendMessage(update.message.chat_id, welcome_text)	
+	menu(bot,update)
 
-def help(bot, update):
-	bot.sendMessage(update.message.chat_id, text=help_text)
+def menu(bot, update):
+	bot.sendMessage(update.message.chat_id, text="Seleziona un comando", reply_markup=menu_keyboard)
 
-def send_luci(bot, update):	
-	custom_keyboard = [[ 'LAMP SX ON', 'LAMP DX ON' ],[ 'LAMP SX OFF', 'LAMP DX OFF' ], ['/foto'],['/video'],  ['OK']]
-	reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-	reply_markup.one_time_keyboard=False
-	reply_markup.resize_keyboard=True
-	bot.sendMessage(update.message.chat_id, text="Seleziona il comando", reply_markup=reply_markup)
+def send_menu_luci(bot, update):	
+	bot.sendMessage(update.message.chat_id, text="Seleziona il comando", reply_markup=luci_keyboard)
 
+def send_menu_foto(bot, update):	
+	bot.sendMessage(update.message.chat_id, text="Seleziona la camera", reply_markup=foto_keyboard)
 
-def send_foto(bot, update):	
-	custom_keyboard = [['CAMERA 1','CAMERA 2','CAMERA 3'],['/luci'],['/video'],[ 'OK' ]]
-	reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-	reply_markup.one_time_keyboard=False
-	reply_markup.resize_keyboard=True
-	bot.sendMessage(update.message.chat_id, text="Seleziona la camera", reply_markup=reply_markup)
+def send_menu_video(bot, update):	
+	bot.sendMessage(update.message.chat_id, text="Seleziona la camera", reply_markup=video_keyboard)
 
-def send_video(bot, update):	
-	custom_keyboard = [['VCAMERA 1','VCAMERA 2','VCAMERA 3'],['/luci'],['/foto'],[ 'OK' ]]
-	reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-	reply_markup.one_time_keyboard=False
-	reply_markup.resize_keyboard=True
-	bot.sendMessage(update.message.chat_id, text="Seleziona la camera", reply_markup=reply_markup)
-
-def send_webimage(bot, update):
+def send_logo(bot, update):
 	bot.sendPhoto(update.message.chat_id, photo='http://www.acmesystems.it/www/tpc/telegram_bulb.jpg')
 
 def send_pdf(bot, update):
 	bot.sendDocument(update.message.chat_id, open('acme.pdf'))
 
-def send_mp3(bot, update):
-	bot.sendAudio(update.message.chat_id, open('idilio.mp3'))
+def send_warner(bot, update):
+	bot.sendAudio(update.message.chat_id, open("That's All Forks 1937.m4a"))
 
 def send_dog(bot, update):
 	os.system("omxplayer -o local angrydog.m4a &")
@@ -101,12 +93,10 @@ def echo(bot, update):
 		#https://github.com/python-telegram-bot/python-telegram-bot#api
 		newFile = bot.getFile(update.message.voice.file_id)
 		newFile.download('voice')
-		os.system("omxplayer -o local voice &")
+		os.system("omxplayer -o local voice")
 
 	if update.message.audio:
 		print " ----> AUDIO <----"
-		#bot.sendVoice(update.message.chat_id, update.message.voice.file_id)
-		#https://github.com/python-telegram-bot/python-telegram-bot#api
 		newFile = bot.getFile(update.message.audio.file_id)
 		newFile.download('audio')
 		os.system("omxplayer -o local audio &")
@@ -115,36 +105,36 @@ def echo(bot, update):
 		print " ----> STICKER <----"
 
 	if update.message.text:
-		if update.message.text=="LAMP SX ON":
+		if update.message.text=="SX ON":
 			GPIO.remove_event_detect(INT_SX)
 			GPIO.output(LAMP_SX, 1)
 			GPIO.add_event_detect(INT_SX, GPIO.BOTH, switch_handler, bouncetime)	
 
-		if update.message.text=="LAMP SX OFF":
+		if update.message.text=="SX OFF":
 			GPIO.remove_event_detect(INT_SX)
 			GPIO.output(LAMP_SX, 0)
 			GPIO.add_event_detect(INT_SX, GPIO.BOTH, switch_handler, bouncetime)	
 
-		if update.message.text=="LAMP DX ON":
+		if update.message.text=="DX ON":
 			GPIO.remove_event_detect(INT_SX)
 			GPIO.output(LAMP_DX, 1)
 			GPIO.add_event_detect(INT_SX, GPIO.BOTH, switch_handler, bouncetime)	
 
-		if update.message.text=="LAMP DX OFF":
+		if update.message.text=="DX OFF":
 			GPIO.remove_event_detect(INT_SX)
 			GPIO.output(LAMP_DX, 0)
 			GPIO.add_event_detect(INT_SX, GPIO.BOTH, switch_handler, bouncetime)	
 
 		if update.message.text=="CAMERA 1":
-			os.system("fswebcam -d /dev/video0 -r 640x360 photo0.jpg")
+			os.system("fswebcam -d /dev/video0 -r 320x240 photo0.jpg")
 			bot.sendPhoto(update.message.chat_id, photo=open('photo0.jpg'))
 
 		if update.message.text=="CAMERA 2":
-			os.system("fswebcam -d /dev/video1 -r 640x360 photo1.jpg")
+			os.system("fswebcam -d /dev/video1 -r 320x240 photo1.jpg")
 			bot.sendPhoto(update.message.chat_id, photo=open('photo1.jpg'))
 
 		if update.message.text=="CAMERA 3":
-			os.system("fswebcam -d /dev/video2 -r 640x360 photo2.jpg")
+			os.system("fswebcam -d /dev/video2 -r 320x240 photo2.jpg")
 			bot.sendPhoto(update.message.chat_id, photo=open('photo2.jpg'))
 
 		#Ancora non funziona
@@ -163,20 +153,20 @@ def main():
 	# Creata l'EventHandler e gli passa il token assegnato al bot
 	# Cambia questo Token con quello che ti ha assegnato BotFather
 	
-	#updater = telegram.Updater("Inserisci qui il Token assegnato da BotFather")	
+	updater = telegram.Updater("Inserisci qui il Token assegnato da BotFather")	
 
 	# Get the dispatcher to register handlers
 	dp = updater.dispatcher
 
 	# Definisce gli handler di gestione dei comandi
 	dp.addTelegramCommandHandler("start", start)
-	dp.addTelegramCommandHandler("help", help)
-	dp.addTelegramCommandHandler("luci", send_luci)
-	dp.addTelegramCommandHandler("foto", send_foto)
-	dp.addTelegramCommandHandler("video", send_video)
-	dp.addTelegramCommandHandler("webimage", send_webimage)
+	dp.addTelegramCommandHandler("menu", menu)
+	dp.addTelegramCommandHandler("luci", send_menu_luci)
+	dp.addTelegramCommandHandler("foto", send_menu_foto)
+	dp.addTelegramCommandHandler("video", send_menu_video)
+	dp.addTelegramCommandHandler("logo", send_logo)
 	dp.addTelegramCommandHandler("pdf", send_pdf)
-	dp.addTelegramCommandHandler("mp3", send_mp3)
+	dp.addTelegramCommandHandler("warner", send_warner)
 	dp.addTelegramCommandHandler("dog", send_dog)
 	dp.addTelegramCommandHandler("mute", send_mute)
 	dp.addTelegramCommandHandler("stop", send_stop)
