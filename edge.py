a4635738f14efff1a7836e40c84a9e99ec9b0dcb
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-01-17 15:31:55 +0000

    Gestione interruttore migliorata

--- edge.py
+++ edge.py
@@ -1,9 +1,6 @@
-#GPIO
-
 import RPi.GPIO as GPIO
 import time
 
-LAMP_DX=17
 LAMP_SX=27
 INT_SX=21
 	
@@ -20,13 +17,12 @@ def main():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 
+	GPIO.setup(LAMP_SX, GPIO.OUT)
+	GPIO.output(LAMP_SX, 0)
+
 	GPIO.setup(INT_SX, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.add_event_detect(INT_SX, GPIO.BOTH, callback=my_callback, bouncetime=100)
 
-	GPIO.setup(LAMP_SX, GPIO.OUT)
-	GPIO.setup(LAMP_DX, GPIO.OUT)
-	GPIO.output(LAMP_SX, 0)
-	GPIO.output(LAMP_DX, 0)
 		
 	try:  
 		
