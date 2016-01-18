d53acf109cb3b193a0ea7daaad75197f5a0a2768
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-01-18 23:19:22 +0000

    Tolte le lampadine

--- edge.py
+++ edge.py
@@ -1,28 +1,43 @@
 import RPi.GPIO as GPIO
 import time
 
-LAMP_SX=27
-INT_SX=21
+LAMP_SX=11
+LAMP_DX=13
+INT_SX=40
+INT_DX=38
 	
-def my_callback(channel):
+def handler_int_sx(channel):
 	print channel
 	if channel==INT_SX:
 		if GPIO.input(LAMP_SX)==0:
 			GPIO.output(LAMP_SX, 1)
 		else:
 			GPIO.output(LAMP_SX, 0)
+
+def handler_int_dx(channel):
+	print channel
+	if channel==INT_DX:
+		if GPIO.input(LAMP_DX)==0:
+			GPIO.output(LAMP_DX, 1)
+		else:
+			GPIO.output(LAMP_DX, 0)
 		
 def main():
 
-	GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BOARD)
 	GPIO.setwarnings(False)
 
 	GPIO.setup(LAMP_SX, GPIO.OUT)
+	GPIO.setup(LAMP_DX, GPIO.OUT)
+	
 	GPIO.output(LAMP_SX, 0)
+	GPIO.output(LAMP_DX, 0)
 
 	GPIO.setup(INT_SX, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(INT_SX, GPIO.BOTH, callback=my_callback, bouncetime=100)
+	GPIO.setup(INT_DX, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+	GPIO.add_event_detect(INT_SX, GPIO.BOTH, callback=handler_int_sx, bouncetime=100)
+	GPIO.add_event_detect(INT_DX, GPIO.BOTH, callback=handler_int_dx, bouncetime=100)
 		
 	try:  
 		
---
+++ mytokens.py
@@ -0,0 +1,5 @@
+#TanzilliBot
+token_string = "145378027:AAH5gtii0NzyGLVixxTbJ5qEr2jlkGkFliI"	
+
+#TanzoBot
+token_string= "174561804:AAEiClkdMEhRSSz8dciyf1joDW-mOnKcZz4"
