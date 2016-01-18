9ffbf8ec7837e4ab9898e95748f5b85802a58bb7
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-01-18 23:20:50 +0000

    Token su file a parte

--- mytokens.py
+++ mytokens.py
@@ -1,5 +1,1 @@
-#TanzilliBot
-token_string = "145378027:AAH5gtii0NzyGLVixxTbJ5qEr2jlkGkFliI"	
-
-#TanzoBot
-token_string= "174561804:AAEiClkdMEhRSSz8dciyf1joDW-mOnKcZz4"
+token_string = "inserisci il tuo token qui"	
