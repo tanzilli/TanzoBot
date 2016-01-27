6139a1f10cf6f5f9f9a27cf58fd38d9c269b1ebc
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-01-27 13:23:31 +0100

    Migliorata la gestione degli accessi

--- tanzocheck.py
+++ tanzocheck.py
@@ -9,21 +9,27 @@ import os.path
 class Check():
 	user_file='user_list.json'
 	user_ids=[]
+	user_usernames=[]
 	user_first_names=[]
+	user_last_names=[]
 	otp=-1
 
 	def __init__(self):
-		print "TanzoCheck 0.2 is fired..."
+		print "TanzoCheck 0.3 is fired..."
 		if os.path.exists(self.user_file):
 			with open(self.user_file) as json_file:
 				json_data=json.load(json_file)
 				self.user_ids=json_data[0]
-				self.user_first_names=json_data[1]
+				self.user_usernames=json_data[1]
+				self.user_first_names=json_data[2]
+				self.user_last_names=json_data[3]
 				
 	def save_user_list(self):
-		user_data=[self.user_ids,self.user_first_names]
+		user_data=[self.user_ids,self.user_usernames,self.user_first_names,self.user_last_names]
 		with open(self.user_file, 'w') as outfile:
-			json.dump(user_data, outfile,indent=4)				
+			json.dump(user_data, outfile,indent=4)		
+		print "Saving user_list.json:"
+		print user_data			
 	
 	#Controlla se l'utente e' uno user autorizzato
 	def user(self,bot,update):
@@ -31,17 +37,20 @@ class Check():
 		#Se non c'e' lista di utenti il primo che accede diventa su
 		if len(self.user_ids)==0:
 			self.user_ids+=[update.message.from_user.id]
+			self.user_usernames+=[update.message.from_user.username]
 			self.user_first_names+=[update.message.from_user.first_name]
+			self.user_last_names+=[update.message.from_user.last_name]
 			self.save_user_list()
 			print "Primo accesso"
-			print "%s diventa diventa super user" % update.message.from_user.first_name
-			print "Lista utenti\n%s %s " % (self.user_ids,self.user_first_names)
+			print "id: %s" % update.message.from_user.id
+			print "username: %s" % update.message.from_user.username
+			print "first_name: %s" % update.message.from_user.first_name
+			print "last_name: %s" % update.message.from_user.last_name
 			return "su"
 
 		#Se lo user e' in lista nella posizione 0 ritorna su
 		if self.user_ids[0]==update.message.from_user.id:
 			print "Accesso dal super user"
-			print "Lista utenti\n%s %s " % (self.user_ids,self.user_first_names)
 			return "su"
 
 		#Se lo user e' in lista torna "user"
@@ -54,15 +63,15 @@ class Check():
 			if update.message.text==self.otp:
 				self.otp=-1
 				self.user_ids+=[update.message.from_user.id]
+				self.user_usernames+=[update.message.from_user.username]
 				self.user_first_names+=[update.message.from_user.first_name]
+				self.user_last_names+=[update.message.from_user.last_name]
 				self.save_user_list()
 				print "Accesso da nuovo user con otp"
-				print "Lista utenti\n%s %s " % (self.user_ids,self.user_first_names)
 				return "user"
 
 		#Negli altri casi nega l'accesso
 		print "Accesso non autorizzato"
-		print "Lista utenti\n%s %s " % (self.user_ids,self.user_first_names)
 		return "none"		
 
 	def cmd_otp(self,bot, update):
@@ -85,13 +94,14 @@ class Check():
 
 		if True:
 			del self.user_ids[1:]
+			del self.user_usernames[1:]
+			del self.user_first_names[1:]
+			del self.user_last_names[1:]
 			bot.sendMessage(update.message.chat_id, "Cancellati tutti gli utenti" )
 			print "Cancellati tutti gli user"
 		else:
 			print "Comando non autorizzato"
 		
-		print "Lista utenti" , self.user_ids
-
 	def addTanzoCheckCommandHandler(self,dispatcher):
 		dispatcher.addTelegramCommandHandler("otp",self.cmd_otp)
 		dispatcher.addTelegramCommandHandler("userdel",self.cmd_userdel)
--- tanzocheck_test.py
+++ tanzocheck_test.py
@@ -19,6 +19,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def messaggi_in_arrivo(bot, update):
+	print "Messaggio in arrivo"
+
 	#Prima di elaborare un messaggio controlla che tipo di utente e':
 	user_type= check.user(bot,update)
 	
@@ -27,14 +29,16 @@ def messaggi_in_arrivo(bot, update):
 		return
 
 	if user_type=="user":
-		bot.sendMessage(update.message.chat_id, "User")
+		bot.sendMessage(update.message.chat_id, "User: [%s]" % update.message.from_user.username)
 		return
 
 	if user_type=="su":
-		bot.sendMessage(update.message.chat_id, "Super user")
+		bot.sendMessage(update.message.chat_id, "Super user: [%s]" % update.message.from_user.username)
 		return
 
 def comando_start(bot, update):	
+	print "Comando start in arrivo"
+
 	#Prima di elaborare un comando controlla che tipo di utente e':
 	user_type= check.user(bot,update)
 	
@@ -43,14 +47,14 @@ def comando_start(bot, update):
 		return
 
 	if user_type=="user":
-		bot.sendMessage(update.message.chat_id, "User: %s" % help_text )
+		bot.sendMessage(update.message.chat_id, "User: [%s]" % update.message.from_user.username)
 		return
 
 	if user_type=="su":
-		bot.sendMessage(update.message.chat_id, "Super user: %s" % help_text)
+		bot.sendMessage(update.message.chat_id, "Super user: [%s]" % update.message.from_user.username)
 		return
 
-updater = Updater(token='415378027:AAGKpq8P6hggT8oAqoWJ6Y_qcXoXJ2Zn96w')
+updater = Updater(token='145378027:AAGKpq8P6hggT8oAqoWJ6Y_qcXoXJ2Zn96w')
 dispatcher = updater.dispatcher
 dispatcher.addTelegramMessageHandler(messaggi_in_arrivo)
 dispatcher.addTelegramCommandHandler("start",comando_start)
