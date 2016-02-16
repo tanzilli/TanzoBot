df1b77829a2184f48833d47d68382a6f93e5efbd
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-02-16 23:51:42 +0100

    Servo pi

--- servo_pi.py
+++ servo_pi.py
@@ -3,21 +3,21 @@ import time
   
 GPIO.setmode(GPIO.BOARD)
   
-GPIO.setup(25, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
  
 ## Set della frequenza a 50 Herz  (Periodo 20 mS)
-servo = GPIO.PWM(25, 50)    
+servo = GPIO.PWM(12, 50)    
 
 ## Imposta il duty cycle al 5% (1 mS on)   
 servo.start(5)             
 
 for i in range(5):
-	time.sleep(1)
+	time.sleep(2)
 
 	## Porta il duty cycle al 10% (2 mS on)   
 	servo.ChangeDutyCycle(10)   
 
-	time.sleep(1)
+	time.sleep(2)
 
 	## Porta il duty cycle al 5% (1 mS on)   
 	servo.ChangeDutyCycle(5)   
